f514eb3faa477c6339362c2c6560d897e1494244
ohhmymaill <45809414+ohhmymaill@users.noreply.github.com>
2018-12-14 12:31:42 +0700

    Minify css js

--- public/class-share-theme-plugin-public.php
+++ public/class-share-theme-plugin-public.php
@@ -73,7 +73,7 @@ class Share_Theme_Plugin_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/share-theme-plugin-public.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/share-theme-plugin-public.min.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +96,7 @@ class Share_Theme_Plugin_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/share-theme-plugin-public.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/share-theme-plugin-public.min.js', array( 'jquery' ), $this->version, false );
 
 	}
 
